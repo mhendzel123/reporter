e81706a2b54e62f51b655f54f5b07afb7d3cbde8
basj2 <bajanik@student.agh.edu.pl>
2020-06-24 19:44:30 +0200

    Report 5

--- src/main/java/pl/edu/agh/mwo/java/App.java
+++ src/main/java/pl/edu/agh/mwo/java/App.java
@@ -15,86 +15,106 @@ import pl.edu.agh.mwo.java.Reports.Raport1;
 import pl.edu.agh.mwo.java.Reports.Raport2;
 import pl.edu.agh.mwo.java.Reports.Raport3;
 import pl.edu.agh.mwo.java.Reports.Raport4;
+import pl.edu.agh.mwo.java.Reports.Raport5;
 import pl.edu.agh.mwo.java.dataObjects.Pracownik;
 import pl.edu.agh.mwo.java.dataObjects.Projekt;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
-		File example = new File("/home/you/Desktop");
+    public static void main(String[] args) throws Exception {
+        File example = new File("/home/you/Desktop");
 
-		Options options = new Options();
+        Options options = new Options();
 
-		Option source = new Option("s", "source", true, "input file path");
-		source.setRequired(true);
-		options.addOption(source);
+        Option source = new Option("s", "source", true, "input file path");
+        source.setRequired(true);
+        options.addOption(source);
 
-		Option report = new Option("r", "report", true, "report type");
-		source.setRequired(true);
-		options.addOption(report);
+        Option report = new Option("r", "report", true, "report type");
+        source.setRequired(true);
+        options.addOption(report);
+        
+        Option report5filter = new Option("r5f", "report5filter", true, "report 5 filter");
+        options.addOption(report5filter);
+        
 
-		CommandLineParser parser = new DefaultParser();
-		HelpFormatter formatter = new HelpFormatter();
-		CommandLine cmd = null;
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
 
-		try {
-			cmd = parser.parse(options, args);
-		} catch (ParseException e) {
-			System.out.println(e.getMessage());
-			formatter.printHelp("utility-name", options);
-			System.exit(1);
-		}
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
 
-		String inputFilePath = cmd.getOptionValue("source");
-		String reportType = cmd.getOptionValue("report");
+        String inputFilePath = cmd.getOptionValue("source");
+        String reportType = cmd.getOptionValue("report");
+        String report5filterString = cmd.getOptionValue("report5filter");
+
+        if (Integer.parseInt(reportType) == 5) {
+            
+        }
 
 //		System.out.println(inputFilePath);
 //		System.out.println(reportType);
 
-		// generowanie listy pracownikow z ich projektami i zadaniami po plikach po
-		// podanej sciezce (rekursywnie)
-		File inputPath = new File(inputFilePath);
-		List<Pracownik> pracownicy = (new WorkbookLoader()).listaPracownikowZFolderu(inputPath);
-		List<Projekt> projekty = (new WorkbookLoader()).listaProjektowZFolderu(inputPath);
-		// tu mozna sprawdzic ze w przypypadku Jana Kowalskiego zadania zostsaly
-		// poprawnie dodane do odpowiadajacych im projektom
-		// oraz ze puste linie sa pomijane (02/Kowalski_Jan ma pusta)
-		pracownicy.forEach((pracownik) -> {
+        // generowanie listy pracownikow z ich projektami i zadaniami po plikach po
+        // podanej sciezce (rekursywnie)
+        File inputPath = new File(inputFilePath);
+        List<Pracownik> pracownicy = (new WorkbookLoader()).listaPracownikowZFolderu(inputPath);
+        List<Projekt> projekty = (new WorkbookLoader()).listaProjektowZFolderu(inputPath);
+        // tu mozna sprawdzic ze w przypypadku Jana Kowalskiego zadania zostsaly
+        // poprawnie dodane do odpowiadajacych im projektom
+        // oraz ze puste linie sa pomijane (02/Kowalski_Jan ma pusta)
+        pracownicy.forEach((pracownik) -> {
 //			 System.out.println(pracownik);
-		});
+        });
 
-		// wypisywanie bledow
-		File[] files = new File(inputFilePath).listFiles();
-		ErrorFinder finder = new ErrorFinder();
-		finder.findErrors(files);
+        // wypisywanie bledow
+        File[] files = new File(inputFilePath).listFiles();
+        ErrorFinder finder = new ErrorFinder();
+        finder.findErrors(files);
 
-		// generowanie raportu nr 1
-		if (Integer.parseInt(reportType) == 1) {
-			Raport1 report1 = new Raport1(pracownicy);
-			report1.printReportConsole();
-			report1.generateReportExcel();
-			report1.generateReportChart();
-		}
+        // generowanie raportu nr 1
+        if (Integer.parseInt(reportType) == 1) {
+            Raport1 report1 = new Raport1(pracownicy);
+            report1.printReportConsole();
+            report1.generateReportExcel();
+            report1.generateReportChart();
+        }
 
-		// generowanie raportu nr 2
-		if (Integer.parseInt(reportType) == 2) {
-			Raport2 report2 = new Raport2(projekty);
-			report2.printReportConsole();
-			report2.generateReportExcel();
-			report2.generateReportChart();
-		}
+        // generowanie raportu nr 2
+        if (Integer.parseInt(reportType) == 2) {
+            Raport2 report2 = new Raport2(projekty);
+            report2.printReportConsole();
+            report2.generateReportExcel();
+            report2.generateReportChart();
+        }
 
-		// generowanie raportu nr 3
-		if (Integer.parseInt(reportType) == 3) {
-			Raport3 report3 = new Raport3(projekty, pracownicy);
-			report3.printReportConsole();
-			report3.generateReportExcel();
-		}
-		
-		if (Integer.parseInt(reportType) == 4) {
+        // generowanie raportu nr 3
+        if (Integer.parseInt(reportType) == 3) {
+            Raport3 report3 = new Raport3(projekty, pracownicy);
+            report3.printReportConsole();
+            report3.generateReportExcel();
+        }
+
+        if (Integer.parseInt(reportType) == 4) {
             Raport4 report4 = new Raport4(projekty);
             report4.printReportConsole();
             report4.generateReportExcel();
         }
-	}
+
+        if (Integer.parseInt(reportType) == 5) {
+            if (report5filterString == null) {
+                System.out.println("Podaj szukane wyrażenie (-r5f)");
+            } else {
+            Raport5 report5 = new Raport5(projekty, report5filterString);
+            report5.printReportConsole();
+            report5.generateReportExcel();
+            }
+        }
+    }
 }
---
+++ src/main/java/pl/edu/agh/mwo/java/Reports/Raport5.java
@@ -0,0 +1,69 @@
+package pl.edu.agh.mwo.java.Reports;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import pl.edu.agh.mwo.java.dataObjects.Projekt;
+import pl.edu.agh.mwo.java.dataObjects.Zadanie;
+
+public class Raport5 implements Raport {
+
+    private List<Projekt> projekty = new ArrayList<>();
+    private String filter;
+
+    public Raport5(List<Projekt> projekty, String filter) {
+        this.projekty = projekty;
+        this.filter = filter.toLowerCase();
+    }
+    
+    public Map<String, Float> generateReport() {
+        Map<String, Float> report = new HashMap<>();
+        for (Projekt projekt : projekty) {
+            String nazwaProjektu = projekt.getNazwa();
+            for (Zadanie zadanie : projekt.getListaZadan()) {
+                String nazwaZadania = zadanie.getNazwaZadania();
+                if (nazwaZadania.toLowerCase().contains(filter)) {
+                    float taskHours = zadanie.getCzasPracy();
+                    String reportKey = nazwaProjektu + '\t' + nazwaZadania;
+                    if (report.containsKey(reportKey)) {
+                        float czasIstniejacegoZadania = report.get(reportKey);
+                        report.put(reportKey, czasIstniejacegoZadania + taskHours);
+                    } else {
+                        report.put(reportKey, taskHours);
+                    }
+                }
+            }
+        }
+        return report;
+    }
+    
+    public float getHoursSum() {
+        float sum = 0;
+        Map<String, Float> report = this.generateReport();
+        for (String task : report.keySet()) {
+            sum += report.get(task);
+        }
+        return sum;
+    }
+    
+    
+    @Override
+    public void printReportConsole() {
+        System.out.println("Raport 5");
+        System.out.println("Szukane wyrażenie: " + filter);
+        System.out.println("Suma godzin: " + this.getHoursSum());
+        Map<String, Float> report = this.generateReport();
+        for (String task : report.keySet()) {
+            System.out.println(task + "\t" + report.get(task));
+        }
+    }
+
+    @Override
+    public void generateReportExcel() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
